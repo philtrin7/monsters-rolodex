54ca160fbb1d6b6dead55df0f6f6003aaa97e776
Philtrin <philtrin@monsters-rolodex>
2019-09-09 11:47:44 +0000

    Updates

---
+++ precache-manifest.87ea080f541aec80d392e0e00bc21ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903a7e2020ba6c4f7e3f7025518f538b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0ca9426c217dd389d221",
+    "url": "/monsters-rolodex/static/css/main.922eaac7.chunk.css"
+  },
+  {
+    "revision": "89375460eeff67c8d98d",
+    "url": "/monsters-rolodex/static/js/2.2752c3e5.chunk.js"
+  },
+  {
+    "revision": "0ca9426c217dd389d221",
+    "url": "/monsters-rolodex/static/js/main.e5754707.chunk.js"
+  },
+  {
+    "revision": "9679b25fbb58365eb7b7",
+    "url": "/monsters-rolodex/static/js/runtime~main.c610535c.js"
+  }
+]);
